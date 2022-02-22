b5df8b2556758ea7d681243a2dc7fc947aafbb4e
Nadya Pod <nadegda.podyacheva@gmail.com>
2022-02-22 17:39:52 +0700

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,20 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let elem = '';
+
+    for (let i = 0; i < expr.length; i += 10) {
+        elem = expr.slice(i, i + 10);
+
+        if (elem === '**********') {
+            result += ' ';
+        } 
+        else {
+            result += MORSE_TABLE[elem.replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-')];
+        }
+    }
+    return result;
 }
 
 module.exports = {
